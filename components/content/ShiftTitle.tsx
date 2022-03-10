d87cda3bfef22649c1e7cc1747784676a69044ef
Suhyun Park <me@shiftpsh.com>
2022-03-10 18:56:40 +0900

    fix: remove isekai portal button

--- components/content/ShiftTitle.tsx
+++ components/content/ShiftTitle.tsx
@@ -1,24 +1,11 @@
 import React from 'react'
 import ContentPadding from '../../layouts/ContentPadding'
 import FullHeightPage from '../../layouts/FullHeightPage'
-import Button from '../Button'
 import Caption from '../Caption'
-import Description from '../Descrption'
 import OuterLink from '../OuterLink'
 import Space from '../Space'
 import Subcaption from '../Subcaption'
 import LeftTimeDisplay from './LeftTimeDisplay'
-
-const katcFormData = {
-  children_search_request_cnt: 1,
-  'search:search_key1:child_search': 'etc_char8',
-  'search:search_key2:child_search': 'etc_char9',
-  'search:search_key3:child_search': 'etc_char1',
-  search_val1: 'MjAyMjAyMTc=',
-  search_val2: 'OTgwODA2',
-  birthDay: '980806',
-  search_val3: '박수현',
-}
 
 const ShiftTitle: React.FC = () => {
   return (
@@ -47,37 +34,10 @@ const ShiftTitle: React.FC = () => {
           <p>
             산업기능요원으로 복무 중 &mdash;{' '}
             <LeftTimeDisplay
-              startAt={new Date("2020-06-22T10:00:00+09:00")}
-              endAt={new Date("2022-05-29T00:00:00+09:00")}
+              startAt={new Date('2020-06-22T10:00:00+09:00')}
+              endAt={new Date('2022-05-29T00:00:00+09:00')}
             />
           </p>
-          {new Date() > new Date("2022-02-17T12:00:00+09:00") ? (
-            <>
-              <p>
-                기초군사훈련 증 &mdash;{' '}
-                <LeftTimeDisplay
-                  startAt={new Date("2022-02-17T14:00:00+09:00")}
-                  endAt={new Date("2022-03-10T14:00:00+09:00")}
-                />{' '}
-              </p>
-              <Space h="1em" />
-              <form
-                target="_blank"
-                method="post"
-                action="https://www.katc.mil.kr/katc/community/children.jsp"
-              >
-                {Object.entries(katcFormData).map(([name, value]) => (
-                  <input type="hidden" key={name} name={name} value={value} />
-                ))}
-                <p>
-                  <Button type="submit">
-                    편지 작성
-                    <Description>, 2022. 2. 22. 12 PM &ndash;</Description>
-                  </Button>
-                </p>
-              </form>
-            </>
-          ) : null}
         </ContentPadding>
       </FullHeightPage>
     </>
