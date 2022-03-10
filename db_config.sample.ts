3e5c9d41c6196cca9ad1d87630228773a28ed7c5
Suhyun Park <me@shiftpsh.com>
2022-03-10 18:55:49 +0900

    fix: db config typo

--- db_config.sample.ts
+++ db_config.sample.ts
@@ -2,7 +2,7 @@ import { SequelizeOptions } from 'sequelize-typescript'
 
 export default {
   dialect: 'mysql',
-  database: '<database name>>',
+  database: '<database name>',
   username: '<database username>',
   password: '<database password>',
 } as SequelizeOptions
