14552af261a8d8d958028421aa14b307bc03bc94
Suhyun Park <me@shiftpsh.com>
2025-10-09 13:53:13 +0900

    feat: translations for /downloads

--- app/downloads/page.tsx
+++ app/downloads/page.tsx
@@ -24,17 +24,18 @@ import CopyButton from "../../components/CopyButton";
 import ColorTable from "./ColorTable";
 import InternalLink from "../../components/InternalLink";
 import ExternalLink from "../../components/ExternalLink";
+import useTranslation from "../../hooks/useTranslation";
+import { translations } from "./translations";
 
 const Page = () => {
+  const { t } = useTranslation(translations);
+
   return (
     <MainContainer>
       <Space h={160} />
-      <Typo h1>자료</Typo>
-      <FullwidthImage
-        src="/img/logo-splash.svg"
-        alt="어두운 바탕과 가운데에 그려져 있는 shiftpsh 로고."
-      />
-      <SectionCaption>로고 및 색상</SectionCaption>
+      <Typo h1>{t.downloads}</Typo>
+      <FullwidthImage src="/img/logo-splash.svg" alt={t.logo_and_colors.alt} />
+      <SectionCaption>{t.logo_and_colors.caption}</SectionCaption>
       <LinksRow>
         <DownloadLink href="/downloads/shiftpsh-logo-svg.zip">
           SVG (8.9KB)
@@ -43,67 +44,57 @@ const Page = () => {
           PNG (265KB)
         </DownloadLink>
         <DownloadLink href="/downloads/shiftpsh-logo-ai-screen.zip">
-          AI (스크린용, 512KB)
+          {t.logo_and_colors.ai_screen}
         </DownloadLink>
         <DownloadLink href="/downloads/shiftpsh-logo-ai-print.zip">
-          AI (인쇄용, 2.2MB)
+          {t.logo_and_colors.ai_print}
         </DownloadLink>
       </LinksRow>
-      <Paragraph>
-        키보드의 Shift 키를 상징하는 화살표와, &lsquo;시프트&rsquo; 및
-        &lsquo;수현&rsquo;의 머리글자 초성 &lsquo;ㅅ&rsquo;를 형상화한 로고를
-        사용하고 있습니다.
-      </Paragraph>
+      <Paragraph>{t.logo_and_colors.description}</Paragraph>
       <ColorTable />
       <Divider />
-      <FullwidthImage
-        src="/img/specs-hanbyeol.png"
-        alt="코랄색 바탕에 흰색 사선 스트라이프가 늘어져 있고, 그 위에 한별이가 그려져 있는 종이를 잘라 얹은 듯한 그림."
-      />
-      <SectionCaption>박한별</SectionCaption>
+      <FullwidthImage src="/img/specs-hanbyeol.png" alt={t.hanbyeol.alt} />
+      <SectionCaption>{t.hanbyeol.caption}</SectionCaption>
       <LinksRow>
         <DownloadLink href="/downloads/hanbyeol-specs.pdf">
-          설정 문서 (577KB)
+          {t.hanbyeol.specs}
         </DownloadLink>
         <InternalLink href="/hanbyeol/guidelines">
-          2차 창작 가이드라인
+          {t.hanbyeol.guidelines}
         </InternalLink>
       </LinksRow>
-      <Paragraph>
-        박한별(한별이)은 인터넷 페르소나 캐릭터입니다. 2014년부터 shiftpsh의
-        인터넷 활동과 함께하고 있습니다.
-      </Paragraph>
+      <Paragraph>{t.hanbyeol.description}</Paragraph>
       <Divider />
-      <SectionCaption>아카이브</SectionCaption>
+      <SectionCaption>{t.archive.caption}</SectionCaption>
       <Itemize>
         <Item>
           <ExternalLink href="https://shiftpsh.com/2021">
-            shiftpsh.com (2021)
+            {t.archive.items["2021"]}
           </ExternalLink>
         </Item>
         <Item>
           <ExternalLink href="https://shiftpsh.com/2019">
-            shiftpsh.com (2019)
+            {t.archive.items["2019"]}
           </ExternalLink>
         </Item>
         <Item>
           <ExternalLink href="https://shiftpsh.com/2017">
-            shiftpsh.com (2017)
+            {t.archive.items["2017"]}
           </ExternalLink>
         </Item>
         <Item>
           <ExternalLink href="https://shiftpsh.com/ed/7/">
-            osu! 스킨 &ndash; SHIFTED 7: VIOLET CLARITY (2016)
+            {t.archive.items.ed_7}
           </ExternalLink>
         </Item>
         <Item>
           <ExternalLink href="https://shiftpsh.com/ed/6/">
-            osu! 스킨 &ndash; SHIFTED 6: EXPRESSIVE HARMONY (2015)
+            {t.archive.items.ed_6}
           </ExternalLink>
         </Item>
         <Item>
           <ExternalLink href="https://shiftpsh.com/ed/5/">
-            osu! 스킨 &ndash; shifted 5: snow dream (2014)
+            {t.archive.items.ed_5}
           </ExternalLink>
         </Item>
       </Itemize>
