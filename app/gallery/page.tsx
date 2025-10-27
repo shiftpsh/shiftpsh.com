9969fb012eade469ce9c96dfe026f73ea6be4ebd
Suhyun Park <me@shiftpsh.com>
2025-10-28 03:36:54 +0900

    feat: add placeholder page for /gallery

---
+++ app/gallery/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+import styled from "@emotion/styled";
+
+const TempPageContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  letter-spacing: 2px;
+`;
+
+const Page = () => {
+  return <TempPageContainer>UNDER CONSTRUCTION</TempPageContainer>;
+};
+
+export default Page;
